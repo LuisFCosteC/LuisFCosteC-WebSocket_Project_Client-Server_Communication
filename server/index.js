257fde261f467f9de41c373046cb54dbd0686d37
LuisFCosteC <luisfcostec@gmail.com>
2024-10-24 14:00:11 -0500

    Socket Handshake is completed

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import logger from 'morgan';
-import dotenv from 'dotenv';
+import express from 'express'
+import logger from 'morgan'
+import dotenv from 'dotenv'
 import { createClient } from '@libsql/client';
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 
+dotenv.config()
 const port = process.env.PORT ?? 3000
 
 const app = express()
@@ -17,23 +18,59 @@ const io = new Server(server, {
 
 // Creamos la conexion a la base de datos
 const db = createClient({
-    url: "libsql://optimum-storm-luisfcostec.turso.io",
+    url: process.env.DB_URL,
     authToken: process.env.DB_TOKEN
 })
 
-io.on('connection', (socket) => {
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        user TEXT
+    )
+`)
+
+io.on('connection', async (socket) => {
     console.log('');
     console.log('A user has connected!')
 
     socket.on('disconnect', () => {
-        console.log('')
+        console.log('');
         console.log('An user has disconnected')
         console.log('');
     })
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
+    socket.on('chat message', async (msg) => {
+        let result
+        const username = socket.handshake.auth.username ?? 'anonymous'
+        console.log({ username, msg })
+        try {
+            result = await db.execute({
+                sql: `INSERT INTO messages (content, user) VALUES (:msg, :username)`,
+                args: { msg, username }
+            })
+        } catch (e) {
+            console.error(e)
+            return
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
     })
+
+    if (!socket.recovered) {
+        try {
+            const results = await db.execute({
+                sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+                args: [socket.handshake.auth.serverOffset ?? 0]
+            })
+
+            results.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString(), row.user)
+            })
+
+        } catch (e) {
+            console.error(e)
+        }
+    }
 })
 
 app.use(logger('dev'))
