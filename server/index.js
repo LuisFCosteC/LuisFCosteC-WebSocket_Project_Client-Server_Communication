520eee1ac7a74cdc47fb4dfd72b55436fb8016ec
LuisFCosteC <luisfcostec@gmail.com>
2024-10-24 11:11:13 -0500

    The websocket is created

--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
 import express from 'express'
 import logger from 'morgan'
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
 
+// Se crea el servidor con HTTP
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', () => {
+    console.log('A user has connected!')
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-    res.send('<h1>This is Chat</h1>')
+    res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
-  console.log(`Server runing on port ${port}`)
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
